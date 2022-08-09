dfd11b5685fe05e6a294781e64eb779bcd2fc31b
ammar97alhalabi <ammar6500@gmail.com>
2022-08-09 14:56:23 +0300

    Enter

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -2,13 +2,13 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState,useEffect } from 'react'
-import h1 from '../../assets/images/Skills/Flutter.svg'
-import h2 from '../../assets/images/Skills/Laravel.svg'
-import h3 from '../../assets/images/Skills/React-icon.svg'
-import h4 from '../../assets/images/Skills/logo1.png'
-import h5 from '../../assets/images/Skills/logo2.png'
-import h6 from '../../assets/images/Skills/logo3.png'
-import h7 from '../../assets/images/Skills/python-logo.svg'
+// import h1 from '../../assets/images/Skills/Flutter.svg'
+// import h2 from '../../assets/images/Skills/Laravel.svg'
+// import h3 from '../../assets/images/Skills/React-icon.svg'
+// import h4 from '../../assets/images/Skills/logo1.png'
+// import h5 from '../../assets/images/Skills/logo2.png'
+// import h6 from '../../assets/images/Skills/logo3.png'
+// import h7 from '../../assets/images/Skills/python-logo.svg'
 // import h8 from '../../assets/images/Skills/'
 // import h9 from '../../assets/images/Skills/'
 
@@ -29,27 +29,27 @@ const Skills=()=>{
                 <div>
                     <ul>
                         <li>
-                        <img src={h1} alt='' />
+                        {/* <img src={h1} alt='' /> */}
                         </li>
                         <li>
-                            <img src={h2} alt='' />
+                            {/* <img src={h2} alt='' /> */}
                         </li>
                         <li>
-                            <img src={h3} alt='' />
+                            {/* <img src={h3} alt='' /> */}
                         </li>
                         <li>
-                            <img src={h7} alt='' />
+                            {/* <img src={h7} alt='' /> */}
                         </li>
                     </ul>
                     <ul>
                         <li>
-                            <img src={h4} alt='' />
+                            {/* <img src={h4} alt='' /> */}
                         </li>
                         <li>
-                            <img src={h5} alt='' />
+                            {/* <img src={h5} alt='' /> */}
                         </li>
                         <li>
-                            <img src={h6} alt='' />
+                            {/* <img src={h6} alt='' /> */}
                         </li>
                         
                     </ul>
